7bac35a8d6444ddb6e0fef58ef19c38f93c25328
nhj7911 <nhj7911@gmail.com>
2020-04-25 23:14:03 +0900

    dp 생각 안하고 1463번 풀어봄

--- src/dp/bj1463.java
+++ src/dp/bj1463.java
@@ -1,10 +1,37 @@
 package dp;
 
+import java.util.Scanner;
+
+/*
+ * bj1463.java
+ * 백준 1463번 : 1로 만들기
+ * version 1.0
+ * data 2020.04.25
+ */
+
 public class bj1463 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test1");
+		
+		Scanner s = new Scanner(System.in);
+		int x = s.nextInt(); // 정수입력받음
+		int count = 0;
+		while(x != 1) {
+			count++;
+			if(x % 3 == 0) {
+				x=x/3;
+			}else if((x-1) % 3 == 0) {
+				x--;
+			}
+			else if (x % 2 == 0) {
+				x=x/2;
+			}else {
+				x--;
+			}
+		}
+		System.out.println(count);
+		
 	}
 
 }
