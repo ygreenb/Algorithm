2e1a75ac23db9cf0603f6beed95daa1207661eb5
nhj7911 <nhj7911@gmail.com>
2020-05-02 14:08:10 +0900

    1149번 주석수정

--- src/dp/bj1149.java
+++ src/dp/bj1149.java
@@ -12,7 +12,6 @@ public class bj1149 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt(); // 집의 수 입력 받음
 		
@@ -33,8 +32,8 @@ public class bj1149 {
 		// 비용의 최솟값 구해서 갱신
 		for(int i=1;i<n;i++) {
 			dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2])+list[i][0]; // i번째에 R을 칠할 경우
-			dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2])+list[i][1]; // i번째에 R을 칠할 경우
-			dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1])+list[i][2]; // i번째에 R을 칠할 경우
+			dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2])+list[i][1]; // i번째에 G을 칠할 경우
+			dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1])+list[i][2]; // i번째에 B을 칠할 경우
 		}
 		
 		System.out.println(Math.min(Math.min(dp[n-1][0],dp[n-1][1]),dp[n-1][2]));
