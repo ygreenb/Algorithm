5725485d9ee1e307190440c34d4ed83164be4c39
Na Hyeji <43837954+ygreenb@users.noreply.github.com>
2023-05-17 22:46:10 +0900

    [Silver IV] Title: 설탕 배달, Time: 120 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -2,25 +2,25 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static int[] arr;
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		arr = new int[N];
-		
-		if (N == 4 || N == 7) {
-			System.out.println(-1);
-		}
-		else if (N % 5 == 0) {
-			System.out.println(N / 5);
-		}
-		else if (N % 5 == 1 || N % 5 == 3) {
-			System.out.println((N / 5) + 1);
-		}
-		else if (N % 5 == 2 || N % 5 == 4) {
-			System.out.println((N / 5) + 2);
-		}
-		
-	}	
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int dp[] = new int[5001];
+    dp[0] = 0;
+    dp[1] = -1;
+    dp[2] = -1;
+    dp[3] = 1;
+    dp[4] = -1;
+    for (int i = 5; i <= n; i++) {
+      if (dp[i - 5] != -1) {
+        dp[i] = dp[i - 5] + 1;
+      } else if (dp[i - 3] != -1) {
+        dp[i] = dp[i - 3] + 1;
+      } else {
+        dp[i] = -1;
+      }
+    }
+    System.out.println(dp[n]);
+  }
 }
