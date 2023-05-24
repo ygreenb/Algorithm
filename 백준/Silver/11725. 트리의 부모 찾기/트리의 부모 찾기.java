0bb6281a0474e13bdf5797191c3bc36b15aa4f72
Na Hyeji <43837954+ygreenb@users.noreply.github.com>
2023-05-25 00:26:52 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1508 ms, Memory: 84696 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+  static private int N;
+  static private List<List<Integer>> adjacencyList;
+  static boolean[] visited;
+  static private int[] parentNode;
+
+  public static void main(String args[]) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    N = Integer.parseInt(br.readLine());
+
+    adjacencyList = new ArrayList<>(N + 1);
+    visited = new boolean[N + 1];
+    parentNode = new int[N + 1];
+
+    for (int i = 0; i <= N; i++) {
+      adjacencyList.add(new ArrayList<>());
+    }
+
+    for (int i = 0; i < N - 1; i++) {
+      String[] input = br.readLine().split(" ");
+      int a = Integer.parseInt(input[0]);
+      int b = Integer.parseInt(input[1]);
+      adjacencyList.get(a).add(b);
+      adjacencyList.get(b).add(a);
+    }
+
+    dfs(1);
+
+    for (int i = 2; i <= N; i++) {
+      System.out.println(parentNode[i]);
+    }
+  }
+
+  public static void dfs(int v) {
+    visited[v] = true;
+
+    for (int neighbor : adjacencyList.get(v)) {
+      if (!visited[neighbor]) {
+        parentNode[neighbor] = v;
+        dfs(neighbor);
+      }
+    }
+  }
+}
